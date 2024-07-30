47c1b25226843633315be73275ed7592d0c9ea9d
Nanda Lal Das <tikludas01@gmail.com>
2024-07-30 12:45:51 +0530

    added contact form yet to add functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { AnimatedTooltipPreview } from "@/components/animatedTooltip";
+import ContactForm from "@/components/ContactForm";
 import Hero from "@/components/Hero";
 import { StickyScrollRevealDemo } from "@/components/StickyScroll";
 import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
@@ -7,7 +8,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="relatice flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"> 
-          <Hero/>   
+          <Hero/> 
+          <ContactForm/> 
     </main>
   );
 }
---
+++ components/ContactForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupFormDemo from './ui/signup-form-demo'
+
+const ContactForm = () => {
+  return (
+    <div>
+        <SignupFormDemo/>
+    </div>
+  )
+}
+
+export default ContactForm
