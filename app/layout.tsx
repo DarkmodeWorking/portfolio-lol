d261dee7cca8226d5f3a6dab36dc358facc44bf6
Nanda Lal Das <tikludas01@gmail.com>
2024-08-01 09:49:52 +0530

    added autoComplete attribute to signup-form

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,5 @@ export default function RootLayout({
         </body>
       </html>
     </>
-
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <main className="relatice flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"> 
           <Hero/> 
+          <StickyScrollRevealDemo/>
           <ContactForm/> 
     </main>
   );
