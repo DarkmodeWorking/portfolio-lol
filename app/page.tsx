b140b500cd9bba75199df935e8cad354c75355cf
Nanda Lal Das <tikludas01@gmail.com>
2024-08-01 11:57:16 +0530

    added projects section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { AnimatedTooltipPreview } from "@/components/animatedTooltip";
 import ContactForm from "@/components/ContactForm";
 import Hero from "@/components/Hero";
 import { StickyScrollRevealDemo } from "@/components/StickyScroll";
+import { RecentProjects } from "@/components/ui/recentProjects";
 import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 import Image from "next/image";
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className="relatice flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"> 
           <Hero/> 
+          <RecentProjects/>
           <StickyScrollRevealDemo/>
           <ContactForm/> 
     </main>
